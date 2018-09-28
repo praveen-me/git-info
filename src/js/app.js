d2137e836e0c10fffed7bdabc062e9b4fb8ed105
praveen-me <praveenkrsaini0@gmail.com>
2018-09-29 01:31:27 +0530

    Work With User data and make elements

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,65 @@
 let user;
 const userName = document.getElementById('user-value');
+const userBlock = document.querySelector('.show_user_details');
 
-// function resolvePromise(response) {
-//   if(response.status === 404) {
-//     Error("User Name Not Found");
-//   } else {
-//     return response.json().
-//     then((res,reject) => {
-//       user = res;
-//     }, (reject) => {
-//       throw new Error("User Name Is Wrong");
-//     });
-//   }
-// }
 
-// function rejectPromise(response) {
-//   if(response.status === 404) {
-//     console.log("Network Error");
-//   } 
-// }
+// function for appending details 
+const showUserDetails = () => {
+  user.userDetails = function() {
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement("div");
+
+    let details = `
+    <div class="user_image">
+      <img src="${this.avatar_url}">
+    </div>
+    <div class="show_details">
+      <p class="show_name">
+      <span class="name_head">Name : </span>
+      <span class="name_value">${this.name}</span>
+      </p>
+      <p class="show_follower">
+        <span class="follower_head">Follower : </span>
+        <span class="follower_value">${this.followers}</span>
+      </p>
+      <p class="show_following">
+        <span class="following_head">Following : </span>
+        <span class="following_value">${this.following}</span>
+      </p>
+      <p class="show_bio">
+        <span class="bio_head">Bio : </span>
+        <span class="bio_value">${this.bio}</span>
+      </p>
+    </div>    
+    `;
+    div.classList.add("details_block");
+    div.innerHTML = details;
+    fragment.appendChild(div);
+    userBlock.innerHTML = '';
+    userBlock.appendChild(fragment);
+  }
+  user.userDetails();
+}
+
+//function for fetchData
+function getData(response) {
+  console.log(response.ok)
+  if(!response.ok) {
+    console.log("User name is not correct");
+    return;
+  } else {
+    return response.json().then(res => {
+      user = res;
+      showUserDetails();
+    });
+  }
+}
 
 //Fetching API
 function fetchData(e) {
   if(e.keyCode === 13) {
-    let data = fetch(`https://api.github.com/users/${userName.value}`).then(response => {
-      console.log(response.ok)
-      if(!response.ok) {
-        console.log("User name is not correct");
-        return;
-      } else {
-        return response.json().then(res => {
-          user = res;
-        });
-      }
-    }).catch(() =>{
+    let data = fetch(`https://api.github.com/users/${userName.value}`).then(getData).
+    catch(() =>{
       console.log("Check your internet connection.");
     });
   }
